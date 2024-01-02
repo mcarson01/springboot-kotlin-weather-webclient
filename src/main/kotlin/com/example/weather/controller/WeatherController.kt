5a98e1ecf36db8800cc7be6c356fee72f3625066
mcarson <matt.b.carson@accenture.com>
2024-01-02 12:20:20 -0700

    added service layer

---
+++ src/main/kotlin/com/example/weather/controller/WeatherController.kt
@@ -0,0 +1,26 @@
+package com.example.weather.controller
+
+import com.example.weather.service.WeatherService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+
+@RestController
+class WeatherController()  {
+
+	@Autowired
+	private lateinit var weatherService: WeatherService
+
+	@GetMapping("/weather")
+	@ResponseBody
+	fun weather(): Mono<Daily> {
+		return weatherService.getWeather()
+	}
+
+}
+
+
+class Daily(val daily: List<Map<String, Any>>)  {}
---
+++ src/main/kotlin/com/example/weather/service/WeatherService.kt
@@ -0,0 +1,33 @@
+package com.example.weather.service
+
+import com.example.weather.controller.Daily
+import com.fasterxml.jackson.databind.JsonNode
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+
+@Service
+class WeatherService {
+
+    fun getWeather(): Mono<Daily> {
+
+        val serviceUrl = "https://api.weather.gov/gridpoints/MLB/33,70/forecast"
+        val client = WebClient.create(serviceUrl)
+
+        val response = client.get()
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(JsonNode::class.java)
+            .map {response -> response.path("properties")}
+            .map {response ->
+                val periodMap = mapOf<String, Any>(
+                    "day_name" to response.path("periods").get(0).path("name"),
+                    "temp_high_celsius" to (response.path("periods").get(0).path("temperature").asInt() - 32) * 5 / 9,
+                    "forecast_blurp" to response.path("periods").get(0).path("shortForecast"),
+                )
+                Daily(listOf(periodMap))
+            }
+        return response
+    }
+}
